781458779458352ae2dd9c429962b9bbac0a1063
Denis Smet <denis.smetannikov@yandex.ru>
2023-03-12 01:12:16 +0400

    New codestyle and PHP 8.1+

--- demo/movies/ExamplesOutput.sh
+++ demo/movies/ExamplesOutput.sh
@@ -1,16 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 #
-# JBZoo Toolbox - Cli
+# JBZoo Toolbox - Cli.
 #
 # This file is part of the JBZoo Toolbox project.
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 #
-# @package    Cli
 # @license    MIT
 # @copyright  Copyright (C) JBZoo.com, All rights reserved.
-# @link       https://github.com/JBZoo/Cli
+# @see        https://github.com/JBZoo/Cli
 #
 
 . demo-magic.sh
--- tests/CliPackageTest.php
+++ tests/CliPackageTest.php
@@ -1,31 +1,29 @@
 <?php
 
 /**
- * JBZoo Toolbox - Cli
+ * JBZoo Toolbox - Cli.
  *
  * This file is part of the JBZoo Toolbox project.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @package    Cli
  * @license    MIT
  * @copyright  Copyright (C) JBZoo.com, All rights reserved.
- * @link       https://github.com/JBZoo/Cli
+ * @see        https://github.com/JBZoo/Cli
  */
 
 declare(strict_types=1);
 
 namespace JBZoo\PHPUnit;
 
-/**
- * Class CliReadmeTest
- *
- * @package JBZoo\PHPUnit
- */
-class CliReadmeTest extends AbstractReadmeTest
+final class CliPackageTest extends \JBZoo\Codestyle\PHPUnit\AbstractPackageTest
 {
-    /**
-     * @var string
-     */
     protected string $packageName = 'Cli';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->excludePaths[] = '.github/assets';
+    }
 }
