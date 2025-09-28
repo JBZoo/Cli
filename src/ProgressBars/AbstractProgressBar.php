7dab3cd409e3d1649e36d277602a44bc86e07c61
SmetDenis <smet.denis@gmail.com>
2025-09-28 22:05:14 +0400

    refactor(psalm): Remove unused return value suppressions
    Removed `@psalm-suppress PossiblyUnusedReturnValue` annotations from various methods as they are no longer necessary.
    Updated `jbzoo/toolbox-dev` development dependency to `^7.3`.

--- src/ProgressBars/AbstractProgressBar.php
+++ src/ProgressBars/AbstractProgressBar.php
@@ -34,9 +34,6 @@ abstract class AbstractProgressBar
     protected ?\Closure $callbackOnStart  = null;
     protected ?\Closure $callbackOnFinish = null;
 
-    /**
-     * @psalm-suppress PossiblyUnusedReturnValue
-     */
     abstract public function execute(): bool;
 
     public function __construct(AbstractOutputMode $outputMode)
